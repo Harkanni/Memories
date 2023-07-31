8b60ed6a6f2db0618ae96ae6025c0a14ed137e9f
harkanni <harkanzyempire001@gmail.com>
2023-07-31 12:14:14 +0100

    App bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
 import { getPost } from './actions/posts';
-import memories from './images/memories.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles';
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   const classes = useStyles();
@@ -20,17 +20,8 @@ const App = () => {
   
   return (
     <Container maxidth='lg'>
-      <AppBar className={classes.appBar} position='static' color='inherit'>
-        <Typography className={classes.heading} variant='h2' align='center'>
-          Memories
-        </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          alt='memories'
-          height='60'
-        ></img>
-      </AppBar>
+      <Navbar />
+
       <Grow in>
         <Container>
           <Grid
